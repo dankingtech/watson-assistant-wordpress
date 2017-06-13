33a0a52c22bd3b05998a957552c3554685a04e84
Hasan Siddiqui <hasan.siddiqui@ibm.com>
2017-06-13 15:21:22 -0400

    Feature 1891: Disable Rate Limiting Settings when ‘No’ selected

--- watson-conversation/includes/settings.js
+++ watson-conversation/includes/settings.js
@@ -2,4 +2,16 @@ jQuery(document).ready(function($) {
     $('#watsonconv_color').wpColorPicker({
       palettes: true
     });
+
+    $('input[name="watsonconv_use_limit"]')
+      .on('change', function() {
+        $('#watsonconv_limit, #watsonconv_interval').attr('disabled', this.value == 'no');
+      })
+      .trigger('change');
+
+    $('input[name="watsonconv_use_client_limit"]')
+      .on('change', function() {
+        $('#watsonconv_client_limit, #watsonconv_client_interval').attr('disabled', this.value == 'no');
+      })
+      .trigger('change');
 });
